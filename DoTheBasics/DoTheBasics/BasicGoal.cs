9524d386bca0b66552c0843ff8074d78258c6e1a
Matthew Jaspers <matthew@matthewjaspers.dev>
2020-07-26 16:23:20 -0400

    Initial commit

---
+++ DoTheBasics/DoTheBasics/BasicGoal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoTheBasics
+{
+    public class BasicGoal
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime AlertTime { get; set; }
+    }
+}
---
+++ DoTheBasics/DoTheBasics/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DoTheBasics
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public List<BasicGoal> BasicsToDo { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BasicsToDo = new List<BasicGoal>();
+            this.BasicsToDo.Add(new BasicGoal { Description = "Take a shower", IsDone = false });
+
+            this.ToDosListView.ItemsSource = this.BasicsToDo;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+    }
+}
